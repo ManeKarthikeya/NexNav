d1b6d666491c6da761b3d0dab02d5bd9b10fe98e
ManeKarthikeya <mane.karthikeya.1811@gmail.com>
2025-08-16 14:33:13 +0530

    twelve

--- app/view-trip/[tripid]/page.tsx
+++ app/view-trip/[tripid]/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 import GlobalMap from '@/app/create-new-trip/_components/GlobalMap';
+//@ts-ignore
 import Itinerary from '@/app/create-new-trip/_components/Itinerary';
 import { Trip } from '@/app/my-trips/page';
 import { useTripDetail, useUserDetail } from '@/app/provider';
